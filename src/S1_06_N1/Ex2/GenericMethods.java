453d497bd361e8f3205329d6dfe3933e80877174
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-04-17 18:19:14 +0200

    N1 Done

--- src/S1_06_N1/Ex2/GenericMethods.java
+++ src/S1_06_N1/Ex2/GenericMethods.java
@@ -1,6 +1,7 @@
 package S1_06_N1.Ex2;
 
 public class GenericMethods {
+    //Standard Class, Generic Method
     static public <T> void printConsole(T t1, T t2, T t3){
         System.out.println(t1 + " " + t2 + " " + t3);
     }
